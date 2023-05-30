27f023e95317fcb0526eedebbe46ecbfbe5e6a4d
furkannyildirimm <furkannyildirimm@hotmail.com>
2023-05-30 19:13:36 +0300

    DetailPageUpdate

--- Dictionary/Dictionary/Search/Controller/SearchViewController.swift
+++ Dictionary/Dictionary/Search/Controller/SearchViewController.swift
@@ -7,12 +7,15 @@
 
 import UIKit
 import DictionaryAPI
+import CoreData
 
 class SearchViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var button: UIButton!
     var keyboardHeight: CGFloat = 0.0
+    var searchEntries: [SearchEntry] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +23,10 @@ class SearchViewController: UIViewController {
         searchBar.returnKeyType = .done
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        loadSearchEntries()
+        tableView.register(UINib(nibName: "SearchCell", bundle: nil), forCellReuseIdentifier: "SearchCell")
     }
+    
     
     @objc func keyboardWillShow(notification: NSNotification) {
         if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
@@ -45,14 +51,67 @@ class SearchViewController: UIViewController {
         performSearch(with: keyword)
     }
     
-    func performSearch(with keyword: String) {
+//    func loadSearchEntries() {
+//        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+//
+//        let fetchRequest: NSFetchRequest<SearchEntry> = SearchEntry.fetchRequest()
+//        fetchRequest.fetchLimit = 6
+//        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "timestamp", ascending: false)]
+//
+//        do {
+//            let fetchedEntries = try context.fetch(fetchRequest)
+//            let uniqueEntries = fetchedEntries.reduce([]) { result, entry in
+//                result.contains(where: { $0.searchText == entry.searchText }) ? result : result + [entry]
+//            }
+//            searchEntries = uniqueEntries
+//            tableView.reloadData()
+//        } catch {
+//            print("Arama girişleri yüklenirken hata oluştu: \(error)")
+//        }
+//    }
+    
+    func loadSearchEntries() {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let fetchRequest: NSFetchRequest<SearchEntry> = SearchEntry.fetchRequest()
+        fetchRequest.fetchLimit = 5 // Son 5 arama girişini almak için sınırlama yapılıyor
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "timestamp", ascending: false)] // Tarih sırasına göre sıralama yapılıyor
+        
+        do {
+            searchEntries = try context.fetch(fetchRequest)
+            tableView.reloadData() // TableView'i güncelle
+        } catch {
+            print("Arama girişleri yüklenirken hata oluştu: \(error)")
+        }
+    }
+    
+    
+    func performSearch(with keyword: String?) {
+        guard let keyword = keyword else {
+            return
+        }
         let dictionaryService = DictionaryService()
         dictionaryService.fetchDictionaryEntries(word: keyword) { [weak self] result in
+            guard let self = self else { return }
+            
             switch result {
             case .success(let entries):
                 DispatchQueue.main.async {
+                    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+                    let searchEntry = SearchEntry(context: context)
+                    searchEntry.searchText = keyword
+                    searchEntry.timestamp = Date()
+                    
+                    do {
+                        try context.save()
+                    } catch {
+                        print("Arama girişi kaydedilirken hata oluştu: \(error)")
+                    }
+                    
+                    self.loadSearchEntries()
+                    
                     let detailViewController = DetailViewController(entries: entries)
-                    self?.navigationController?.pushViewController(detailViewController, animated: true)
+                    self.navigationController?.pushViewController(detailViewController, animated: true)
                 }
             case .failure(let error):
                 DispatchQueue.main.async { [weak self] in
@@ -64,6 +123,7 @@ class SearchViewController: UIViewController {
             }
         }
     }
+    
 }
 
 extension SearchViewController: UISearchBarDelegate {
@@ -84,3 +144,22 @@ extension SearchViewController: UISearchBarDelegate {
         searchBar.resignFirstResponder()
     }
 }
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchEntries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchCell", for: indexPath) as! SearchCell
+        let searchEntry = searchEntries[indexPath.row]
+        cell.titleLabel.text = searchEntry.searchText
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let searchEntry = searchEntries[indexPath.row]
+        let keyword = searchEntry.searchText
+        performSearch(with: keyword)
+    }
+}
+
---
+++ Dictionary/Dictionary/SearchCell.swift
@@ -0,0 +1,25 @@
+//
+//  SearchCell.swift
+//  Dictionary
+//
+//  Created by STARK on 30.05.2023.
+//
+
+import UIKit
+
+class SearchCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
